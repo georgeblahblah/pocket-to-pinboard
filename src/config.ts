8b40d1fe5d6db29a9b8104b899630c2c64ea2e43
George B <705427+georgeblahblah@users.noreply.github.com>
2022-03-28 18:58:54 +0100

    Fetch config from AWS SSM

--- src/config.ts
+++ src/config.ts
@@ -1,16 +1,52 @@
-const pocketAccessToken = process.env.POCKET_ACCESS_TOKEN || "";
-const pocketConsumerKey = process.env.POCKET_CONSUMER_KEY || "";
-const pinboardToken = process.env.PINBOARD_TOKEN || "";
+import { SSMClient, GetParametersCommand } from "@aws-sdk/client-ssm";
 
-const config = new Map<string, string>();
-config.set("pocketAccessToken", pocketAccessToken);
-config.set("pocketConsumerKey", pocketConsumerKey);
-config.set("pinboardToken", pinboardToken);
+type Config = Record<string, string>;
+let state: Config;
 
-export const getConfigItem = (key: string): Promise<string> => {
-  const configItem = config.get(key);
-  if (configItem) {
-    return Promise.resolve(configItem);
+const AWS_REGION = "eu-west-2";
+const parameterPathPrefix = "/pocketToPinboard/";
+
+const makeParameterPaths = (parameterNames: string[]): string[] =>
+  parameterNames.map(
+    (parameterName) => `${parameterPathPrefix}${parameterName}`
+  );
+
+const ssmClient = new SSMClient({
+  region: AWS_REGION,
+});
+
+const parameterPaths = makeParameterPaths([
+  "pocketAccessToken",
+  "pocketConsumerKey",
+  "pinboardToken",
+]);
+
+const fetchConfigFromSSM = async (): Promise<Config> => {
+  if (state) {
+    return Promise.resolve(state);
+  }
+  const command = new GetParametersCommand({
+    Names: parameterPaths,
+    WithDecryption: true,
+  });
+
+  const response = await ssmClient.send(command);
+  response.Parameters?.forEach((parameter) => {
+    const maybeParameterName = parameter.Name;
+    if (maybeParameterName !== undefined) {
+      state[maybeParameterName] = parameter.Value!.replace(
+        parameterPathPrefix,
+        ""
+      );
+    }
+  });
+  return state;
+};
+
+export const getConfigItem = async (key: string): Promise<string> => {
+  const conf: Config = await fetchConfigFromSSM();
+  if (conf[key]) {
+    return conf[key];
   } else {
     throw new Error(`No value for key ${key}`);
   }
